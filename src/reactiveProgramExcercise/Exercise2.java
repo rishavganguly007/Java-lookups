6113a4d98da8a2036ffd6abef7bb9925f1ac06fe
rishavganguly007 <rishavganguly007@gmail.com>
2023-07-29 20:46:05 +0530

    completed excercise 2,3,4

--- src/reactiveProgramExcercise/Exercise2.java
+++ src/reactiveProgramExcercise/Exercise2.java
@@ -9,11 +9,18 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        /*
+            This basically means that, it will wait for an event to trigger and then it will subscribe
+            to sysout to print the values
+         */
+        ReactiveSources.intNumbersFlux().subscribe(e -> System.out.println(e));
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
-
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
+        /*
+            Now if the below 2 codes are cmmented the below there will be no output as the process ends
+            without waiting for any event to trigger, you can also use threads.sleep to implement this
+         */
         System.out.println("Press a key to end");
         System.in.read();
     }
--- src/reactiveProgramExcercise/Exercise3.java
+++ src/reactiveProgramExcercise/Exercise3.java
@@ -1,6 +1,8 @@
 package src.reactiveProgramExcercise;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise3 {
 
@@ -10,8 +12,15 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> nums = ReactiveSources.intNumbersFlux().toStream().collect(Collectors.toList());
+        // This ln will run after a delay of 1 * n secs as it is a blocking operation, ie it will hold untill its has everything
+        System.out.println("List is " + nums );
+        System.out.println("Size " + nums.size());
 
+        /*
+        Flux is a async seq 0 -> n items
+        Mono is a async seq 0 or 1 item
+         */
 
         System.out.println("Press a key to end");
         System.in.read();
