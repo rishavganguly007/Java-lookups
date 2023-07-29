95015c145f6fade814d8cbe402a47610625f6661
rishavganguly007 <rishavganguly007@gmail.com>
2023-07-29 18:10:53 +0530

    completed excercise 1

--- src/reactiveProgramExcercise/Exercise1.java
+++ src/reactiveProgramExcercise/Exercise1.java
@@ -1,5 +1,7 @@
 package src.reactiveProgramExcercise;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Exercise1 {
@@ -9,26 +11,37 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
         StreamSources src = new StreamSources();
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+
         Stream<Integer> st = src.intNumbersStream();
-       // st.forEach(s -> System.out.println(s));
+       // st.forEach(s -> System.out.print(s));
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
-        st.filter(i -> i < 5).forEach(s -> System.out.println(s));
+
+        // st.filter(i -> i < 5).forEach(s -> System.out.println(s));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        // st.filter(i -> i > 5).limit(2).forEach(s -> System.out.println(s));
+
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+//        Integer s = st.filter(i -> i > 5).
+//                findFirst().
+//                orElse(-1);
+//        System.out.println(s);
+
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+
+        List<Integer> stList =  st.collect(Collectors.toList());
+        Stream<User> user = src.userStream();
+            user.
+                filter(i -> stList.contains(i.getId())).
+                map(User::getFirstName).
+                forEach(System.out::println);
 
     }
 
