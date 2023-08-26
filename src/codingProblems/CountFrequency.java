c5de844406dce7cc0dbe12b64f4bbaa50c227d30
rishavganguly007 <rishavganguly007@gmail.com>
2023-08-26 20:49:50 +0530

    added code

--- src/codingProblems/CountFrequency.java
+++ src/codingProblems/CountFrequency.java
@@ -1,5 +1,25 @@
 package src.codingProblems;
-
+import java.util.*;
 public class CountFrequency {
 
+    public static void count(String str) {
+        Map<Character, Integer> mp = new HashMap<>();
+
+        for(int i = 0; i < str.length(); i++) {
+            Character s = str.charAt(i);
+            if (mp.containsKey(s)) {
+                mp.put(s, mp.get(s) + 1);
+            }
+            else
+                mp.put(s, 1);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        count("hello");
+
+    }
 }
