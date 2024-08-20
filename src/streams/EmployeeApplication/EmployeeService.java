634c537c46ce49b2a8b835f1c2b2cafe285c35e2
rishavganguly007 <rishavganguly007@gmail.com>
2024-08-20 22:30:44 +0530

    to practise streams

--- src/streams/EmployeeApplication/EmployeeService.java
+++ src/streams/EmployeeApplication/EmployeeService.java
@@ -12,36 +12,36 @@ public class EmployeeService {
     public void findSumOfSalary()
     {
         emp.stream().forEach(e -> System.out.println("Name " + e.getFirstName() + " Salary " + e.getSalary()));
-        Double sum = emp.stream()
+        /*Double sum = emp.stream()
                 .mapToDouble(e -> e.getSalary())
                 .sum();
-        System.out.println("total salary: " + sum);
+        System.out.println("total salary: " + sum);*/
     }
 
     public void listDepartmentNameAndCount()
     {
-       Map<String, Long> departmentCount =  emp.stream()
+       /*Map<String, Long> departmentCount =  emp.stream()
                .map(Employee::getDepartment)
                .collect(Collectors.groupingBy(Department::getDepartmentName, Collectors.counting()));
 
-       departmentCount.forEach((departmentName, count) -> System.out.println(departmentName + ": " + count ));
+       departmentCount.forEach((departmentName, count) -> System.out.println(departmentName + ": " + count ));*/
     }
 
     public void findSeniorMostEmployee()
     {
-        Optional<Employee> e = emp.stream()
+        /*Optional<Employee> e = emp.stream()
                 .min((e1, e2) -> e1.getHireDate().compareTo(e2.getHireDate()));
 
         if(e.isPresent())
         {
             Employee seniorEmp = e.get();
             System.out.println("Name: " + seniorEmp.getFirstName() + " " + "HireDate: " + seniorEmp.getHireDate());
-        }
+        }*/
     }
 
     public void findEmpNameAndDuration()
     {
-        List<String> e = emp.stream()
+        /*List<String> e = emp.stream()
                 .map(em ->{
                     String name = em.getFirstName();
                     LocalDate hireDate = em.getHireDate();
@@ -51,12 +51,12 @@ public class EmployeeService {
                     return "Name : " + name + ", Duration: " + month + " months " + day + " days";
                 })
                 .collect(Collectors.toList());
-        e.forEach(System.out :: println);
+        e.forEach(System.out :: println);*/
     }
 
     public void sortByEmpId()
     {
-        emp.stream()
+        /*emp.stream()
                 .sorted((e1, e2) -> (e1.getEmployeeId().compareTo(e2.getEmployeeId())))
                 .forEach(e -> System.out.println("ID : " + e.getEmployeeId() + " Name: " + e.getFirstName()));
 
@@ -64,21 +64,21 @@ public class EmployeeService {
 
         emp.stream()
                 .sorted(Comparator.comparing(Employee::getEmployeeId).reversed())
-                .forEach(e -> System.out.println("ID : " + e.getEmployeeId() + " Name: " + e.getFirstName()));
+                .forEach(e -> System.out.println("ID : " + e.getEmployeeId() + " Name: " + e.getFirstName()));*/
     }
 
     public void sortByDeptId()
     {
-        emp.stream()
+        /*emp.stream()
                 .map(Employee::getDepartment)
                 .sorted(Comparator.comparing(Department::getDepartmentId))
-                .forEach(d -> System.out.println("Dept Id: " + d.getDepartmentId() + " Name : " + d.getDepartmentName()));
+                .forEach(d -> System.out.println("Dept Id: " + d.getDepartmentId() + " Name : " + d.getDepartmentName()));*/
     }
 
     public void sortByName()
     {
-        emp.stream()
+        /*emp.stream()
                 .sorted(Comparator.comparing(Employee::getFirstName))
-                .forEach(e -> System.out.println("Name : " + e.getFirstName() + " " + e.getLastName()));
+                .forEach(e -> System.out.println("Name : " + e.getFirstName() + " " + e.getLastName()));*/
     }
 }
--- src/streams2/employeeApplication2/EmployeeService.java
+++ src/streams2/employeeApplication2/EmployeeService.java
@@ -14,23 +14,23 @@ public class EmployeeService {
     public void getAllDistinctProjects() {
 
         System.out.println("1. List all distinct project in non-ascending order.");
-        List<Project> project = getAllEmployees()
+        /*List<Project> project = getAllEmployees()
                                     .stream()
                                     .map(Employee::getProjects)
                                     .flatMap(Collection::stream)
                                     .distinct()
                                     .sorted(Comparator.comparing(Project::getName))
                                     .toList();
-        System.out.println(Arrays.toString(project.toArray()));
+        System.out.println(Arrays.toString(project.toArray()));*/
     }
 
     public void getAllNameStartsWith(String initial) {
         System.out.println("2. Print full name of any employee whose firstName starts with ‘A’");
 
-        getAllEmployees()
+        /*getAllEmployees()
                 .stream()
                 .filter(f -> f.getFirstName().startsWith(initial))
-                .forEach(r -> System.out.println(r.getId() + " " + r.getFirstName() +  " " + r.getLastName()));
+                .forEach(r -> System.out.println(r.getId() + " " + r.getFirstName() +  " " + r.getLastName()));*/
 
    }
 
@@ -39,11 +39,11 @@ public class EmployeeService {
         /*getAllEmployees()
                 .forEach(r -> System.out.println(r.getId() + " " + r.getFirstName() +  " " + r.getLastName()));
         System.out.println("break");*/
-        getAllEmployees()
+        /*getAllEmployees()
                 .stream()
                 .filter(r ->
                     r.getId().startsWith(year))
                 .distinct()
-                .forEach(r -> System.out.println(r.getId() + " " + r.getFirstName() +  " " + r.getLastName()));
+                .forEach(r -> System.out.println(r.getId() + " " + r.getFirstName() +  " " + r.getLastName()));*/
     }
 }
